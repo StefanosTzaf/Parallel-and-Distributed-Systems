a2861350c59ba16d3c9e0a1cc9278b0ea86352fe
eeeleftheria <eleftheria.galiatsatou@email.com>
2025-11-23 14:52:06 +0200

    1_2 first 2 methods

---
+++ 1_2.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define perror2(s, e) fprintf(stderr ,"%s: %s\n", s, strerror(e))
+
+
+int sharedVar;
+pthread_mutex_t mutex;
+pthread_rwlock_t rwlock;
+
+void* threadFunc_mutex(void*);
+void* threadFunc_rwlock(void*);
+void* threadFunc_atomic(void*);
+
+int main(int argc, char* argv[]){
+    
+    if(argc != 4){
+        printf("Usage: %s number_of_threads number_of_iterations num_of_algorithm\n", argv[0]);
+        exit(1);
+    }
+    
+    sharedVar = 0;
+    
+    int threadsNum = atoi(argv[1]);
+    int numOfIter = atoi(argv[2]);
+    int algorithm = atoi(argv[3]);
+
+    pthread_t* threads = malloc(threadsNum*sizeof(pthread_t));
+
+    void* (*threadFunc)(void*); // function pointer to thread function
+
+    switch (algorithm){
+        case 1:
+            printf("---Using mutex lock---\n");
+            pthread_mutex_init(&mutex, NULL);
+            threadFunc = threadFunc_mutex;
+            break;
+        case 2:
+            printf("---Using rwlock---\n");
+            pthread_rwlock_init(&rwlock, NULL);
+            threadFunc = threadFunc_rwlock;
+            break;
+        case 3:
+            printf("---Using atomic operations---\n");
+            threadFunc = threadFunc_atomic;
+            break;
+        default:
+            printf("Invalid algorithm number: Use 1 for mutex, 2 for rwlock, 3 for atomic.\n");
+            exit(1);
+    }
+    
+
+    int err, status;
+    for(int i = 0; i < threadsNum; i++){
+        if((err = pthread_create(&threads[i], NULL, threadFunc, (void*)&numOfIter))){
+            perror2("pthread_create", err);
+            exit(1);
+        }
+    }
+
+    for(int i = 0; i < threadsNum; i++){
+        if((err = pthread_join(threads[i], (void**)&status))){
+            perror2("pthread_join", err);
+            exit(1);
+        }
+    }
+
+    switch (algorithm){
+        case 1:
+            pthread_mutex_destroy(&mutex);
+            break;
+        case 2:
+            pthread_rwlock_destroy(&rwlock);
+            break;
+        case 3:
+            // no destruction needed for atomic
+            break;
+        default:
+            break;
+    }                   
+    
+    free(threads);
+    return 0;
+}
+
+
+void* threadFunc_mutex(void* arg){
+    int iters = *(int*)arg;
+
+    for(int i = 0; i < iters; i++){
+        pthread_mutex_lock(&mutex);
+        sharedVar++;
+        printf("in thread %ld with sharedVal: %d\n", pthread_self(), sharedVar);
+        pthread_mutex_unlock(&mutex);
+    }
+    pthread_exit(NULL);
+}
+
+void* threadFunc_rwlock(void* arg){
+    int iters = *(int*)arg;
+
+    for(int i = 0; i < iters; i++){
+        // write lock for incrementing sharedVar
+        pthread_rwlock_wrlock(&rwlock);
+        sharedVar++;
+        pthread_rwlock_unlock(&rwlock);
+        
+        // read lock for printing only
+        pthread_rwlock_rdlock(&rwlock);
+        printf("in thread %ld with sharedVal: %d\n", pthread_self(), sharedVar);
+        pthread_rwlock_unlock(&rwlock);
+    }
+    pthread_exit(NULL);
+}
+
+void* threadFunc_atomic(void* arg){
+
+    pthread_exit(NULL);
+}
--- Makefile
+++ Makefile
@@ -1,11 +1,23 @@
-OBJS = 1_1.o 
 CC = gcc
 CFLAGS = -Wall -g -pthread
-TARGET = 1_1
 
+OBJ1 = 1_1.o 
+OBJ2 = 1_2.o
 
+TARGET1 = 1_1
+TARGET2 = 1_2
 
-$(TARGET): $(OBJS)
-	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
+all: $(TARGET1) $(TARGET2)
+
+$(TARGET1): 1_1.o
+	$(CC) $(CFLAGS) -o $(TARGET1) $(OBJ1)
+
+$(TARGET2): 1_2.o
+	$(CC) $(CFLAGS) -o $(TARGET2) $(OBJ2)
+
+# Run targets with example arguments
+run2:
+	./$(TARGET2) 3 3 2
+
 clean:
-	rm -f $(TARGET) $(OBJS)
+	rm -f $(TARGET1) $(OBJ1) $(TARGET2) $(OBJ2)
