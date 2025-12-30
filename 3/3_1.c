beaf2753f5cf4e59107a76099de2c72762f0ab9d
StefanosTzaf <stzaferis04@gmail.com>
2025-12-30 23:50:45 +0200

    3_1 working

--- 3/3_1.c
+++ 3/3_1.c
@@ -5,9 +5,9 @@
 #include <stdbool.h>
 
 // Function to verify results
-bool verify_results(__int128_t* serial, __int128_t* parallel, int size) {
-    for(int i = 0; i < size; i++) {
-        if(serial[i] != parallel[i]) {
+bool verify_results(__int128_t* serial, __int128_t* parallel, int size){
+    for(int i = 0; i < size; i++){
+        if(serial[i] != parallel[i]){
             printf("Mismatch at coefficient %d:\n", i);
             return false;
         }
@@ -17,26 +17,36 @@ bool verify_results(__int128_t* serial, __int128_t* parallel, int size) {
 
 
 int main(int argc, char *argv[]){
-    int my_rank;
+
     MPI_Init(NULL, NULL);
+    int my_rank, nprocs;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    if(my_rank == 0) {
-        if (argc != 2) {
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (argc != 2){
+        if(my_rank == 0){
             printf("Usage: %s <Degree of the Polynomials>\n", argv[0]);
-            return 1;
         }
-        int degree = atoi(argv[1]);
-        int* poly1 = (int*)malloc((degree + 1) * sizeof(int));
-        if(poly1 == NULL){
-            fprintf(stderr, "Memory allocation failed\n");
-            return 1;
-        }
-        int* poly2 = (int*)malloc((degree + 1) * sizeof(int));
-        if(poly2 == NULL){
-            fprintf(stderr, "Memory allocation failed\n");
-            return 1;
-        }
-        
+        MPI_Finalize();
+        return 1;
+    }
+
+    int degree = atoi(argv[1]);
+
+    int* poly1 = (int*)malloc((degree + 1) * sizeof(int));
+    if(poly1 == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
+    }
+    int* poly2 = (int*)malloc((degree + 1) * sizeof(int));
+    if(poly2 == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
+    }
+    
+    if (my_rank == 0){
         // Initialize polynomials with random coefficients
         struct timespec ts;
         clock_gettime(CLOCK_REALTIME, &ts);
@@ -52,54 +62,154 @@ int main(int argc, char *argv[]){
             bool sign = rand_r(&seed) % 2;
             poly2[i] = sign ? -value : value;
         }
-    
-        int result_size = degree * 2 + 1;
-        __int128_t* serial_result = calloc(result_size, sizeof(__int128_t));
-        if(serial_result == NULL){
-            fprintf(stderr, "Memory allocation failed\n");
-            return 1;
+    }
+
+    int result_size = degree * 2 + 1;
+    int coeffs_per_process = result_size / nprocs;
+    int rem = result_size % nprocs;
+
+    // check if this process will take 1 more coefficient
+    int my_coeffs = coeffs_per_process + (my_rank < rem ? 1 : 0);
+    // if we are in a process that takes extra coefficient, we need to adjust the start index
+    int start_coeff = my_rank * coeffs_per_process + (my_rank < rem ? my_rank : rem);
+
+
+    __int128_t *local_res = (__int128_t*)calloc((size_t)my_coeffs, sizeof(__int128_t));
+    if (!local_res) {
+        fprintf(stderr, "Rank %d: Memory allocation failed for local_res\n", my_rank);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    __int128_t *parallel_result = NULL;
+    __int128_t *serial_result = NULL;
+
+    if (my_rank == 0){
+        parallel_result = (__int128_t*)calloc((size_t)result_size, sizeof(__int128_t));
+        serial_result   = (__int128_t*)calloc((size_t)result_size, sizeof(__int128_t));
+        if (parallel_result == NULL || serial_result == NULL){
+            fprintf(stderr, "Root: Memory allocation failed for results\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
         }
+    }
+
+    //--------------Timings (without init)--------------
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double total_start = MPI_Wtime();
+    double send_start = MPI_Wtime();
+    // we do not have to consider synchronous and asynchronous sends because broadcast sends and receives the data 
+   //TODO : do we need to send all the data????
+    MPI_Bcast(poly1, degree + 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(poly2, degree + 1, MPI_INT, 0, MPI_COMM_WORLD);
+    double send_end = MPI_Wtime();
+    double send_time_local = send_end - send_start;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double parallel_start = MPI_Wtime();
+    // Parallel Polynomial Multiplication (same as pthread logic but here the first loop starts always
+    // from 0 because it saves results in local buffer)
+    for(int k = 0; k < my_coeffs; k++){
+        int global_k = start_coeff + k;
+        __int128_t sum = 0;
         
-        __int128_t* parallel_result = calloc(result_size, sizeof(__int128_t));
-        if(parallel_result == NULL){
-            fprintf(stderr, "Memory allocation failed\n");
-            return 1;
+        int i_start = (global_k - degree) > 0 ? (global_k - degree) : 0;
+        int i_end = (global_k < degree) ? global_k : degree;
+        
+        for (int i = i_start; i <= i_end; i++){
+            sum += (__int128_t)poly1[i] * poly2[global_k - i];
         }
+
+        local_res[k] = sum;
+    }
+    double parallel_end = MPI_Wtime();
+    // parallel time for this process (remember this will run concurrently with others)
+    double parallel_time_local = parallel_end - parallel_start;
     
-        
-        double serial_start, serial_end;
-        // start serial algorithm timing
-        serial_start = MPI_Wtime();
-        
-        //Serial Polynomial Multiplication
+    MPI_Barrier(MPI_COMM_WORLD);
+    double gather_start = MPI_Wtime();
+
+    int bytes_to_send = (int)(my_coeffs * sizeof(__int128_t));
+
+    // saves how many bytes each process will send to root
+    int* recv_counts = NULL;
+    // saves the displacements in the final array where each process data will go
+    int* displs = NULL;
+
+    if (my_rank == 0){
+
+        recv_counts = (int*)malloc(nprocs * sizeof(int));
+        displs = (int*)malloc(nprocs * sizeof(int));
+        if (recv_counts == NULL || displs == NULL) {
+            fprintf(stderr, "Root: Memory allocation failed for recv_counts or displs\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        // prepare recv_counts and displs arrays before gathering
+        for(int i = 0; i < nprocs; i++){
+            int proc_coeffs = coeffs_per_process + (i < rem ? 1 : 0);
+            int start_idx = i * coeffs_per_process + (i < rem ? i : rem);
+            recv_counts[i] = proc_coeffs * sizeof(__int128_t);
+            displs[i] = start_idx * sizeof(__int128_t);
+        }
+    }
+
+    MPI_Gatherv(local_res, bytes_to_send, MPI_BYTE,
+                parallel_result, recv_counts, displs, MPI_BYTE,
+                0, MPI_COMM_WORLD);
+
+    double gather_end = MPI_Wtime();
+    double total_end = MPI_Wtime();
+    double gather_time_local = gather_end - gather_start;
+    double total_time_local = total_end - total_start;
+
+    // reduction to find max times among all processes
+    double max_send_time, max_parallel_time, max_gather_time, max_total_time;
+    MPI_Reduce(&send_time_local, &max_send_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&parallel_time_local, &max_parallel_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&gather_time_local, &max_gather_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&total_time_local, &max_total_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == 0){
+        // Serial Polynomial Multiplication for verification
+        double serial_start = MPI_Wtime();
+
         for(int k = 0; k < result_size; k++) {
             __int128_t sum = 0;
             
             int i_start = (k - degree) > 0 ? (k - degree) : 0;
             int i_end = (k < degree) ? k : degree;
             
-            // For each coefficient k of the result:
-            // We need to add all products poly1[i] * poly2[j] where i+j=k
             for (int i = i_start; i <= i_end; i++) {
                 sum += (__int128_t)poly1[i] * poly2[k - i];
             }
-            
             serial_result[k] = sum;
         }
-        serial_end = MPI_Wtime();
-        printf("Serial Polynomial Multiplication Time: %f seconds\n", serial_end - serial_start);
-        free(poly1);
-        free(poly2);
+
+        double serial_end = MPI_Wtime();
+        double serial_time = serial_end - serial_start;
+
+        if (verify_results(serial_result, parallel_result, result_size)){
+            printf("Results match!\n");
+        } 
+        else{
+            printf("Results do not match!\n");
+        }
+
+        // Print timings
+        printf("Serial Computation Time: %f seconds\n\n", serial_time);
+        printf("Max Send Time: %f seconds\n", max_send_time);
+        printf("Max Parallel Computation Time: %f seconds\n", max_parallel_time);
+        printf("Max Gather Time: %f seconds\n", max_gather_time);
+        printf("Max Total Time: %f seconds\n", max_total_time);
+
+        free(recv_counts);
+        free(displs);
+        free(parallel_result);
         free(serial_result);
-
     }
-    else{
-        
-    }
-
-
-
-    
+    free(local_res);
+    free(poly1);
+    free(poly2);
     MPI_Finalize();
     return 0;
 }
